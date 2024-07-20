d4642830f36bd78663bb185ee77c45476f37fe32
Chase Struse <chasestruse@gmail.com>
2024-07-20 16:18:14 -0700

    End of video

---
+++ PhoenixGameEngine/src/test.cpp
@@ -0,0 +1,10 @@
+#include "test.h"
+#include <stdio.h>
+
+namespace Phoenix {
+	
+	void Print() {
+		printf("Welcome to Phoenix Engine");
+	}
+
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,10 @@
+namespace Phoenix {
+
+	__declspec(dllimport) void Print();
+
+}
+void main() {
+
+	Phoenix::Print();
+
+}
