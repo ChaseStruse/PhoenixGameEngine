c6f09570bbb592cdec2a63b2c59b16187cf9ba7f
Chase Struse <chasestruse@gmail.com>
2024-07-21 09:45:52 -0700

    Implemented logger macros

--- PhoenixGameEngine/src/Phoenix/Log.h
+++ PhoenixGameEngine/src/Phoenix/Log.h
@@ -20,3 +20,16 @@ namespace Phoenix {
 	};
 };
 
+// Core Log Macros
+#define PHOENIX_CORE_FATAL(...) ::Phoenix::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define PHOENIX_CORE_ERROR(...) ::Phoenix::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define PHOENIX_CORE_WARN(...)  ::Phoenix::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define PHOENIX_CORE_INFO(...)  ::Phoenix::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define PHOENIX_CORE_TRACE(...) ::Phoenix::Log::GetCoreLogger()->trace(__VA_ARGS__)
+
+// Client Log Macros
+#define CORE_FATAL(...)         ::Phoenix::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define CORE_ERROR(...)         ::Phoenix::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CORE_WARN(...)          ::Phoenix::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CORE_INFO(...)          ::Phoenix::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CORE_TRACE(...)         ::Phoenix::Log::GetClientLogger()->trace(__VA_ARGS__)
